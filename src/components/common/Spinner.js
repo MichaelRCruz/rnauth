a8ff4a727f0085586770b12651331e560c7bafcc
Michael Cruz <michael.cruz@uwalumni.com>
2017-09-12 19:03:12 -0700

    Spinner compinent

--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 
-const Spinner = () => {
+const Spinner = ({ size }) => {
   return (
-    <View />
+    <View style={spinnerStyle}>
+      <ActivityIndicator size={size || 'large'} />
+    </View>
   );
-}
+};
+
+const styles = {
+  spinnerStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+};
